53d288cec372b81910ca9f1ff358f8ee9103841a
Corey Halpin <chalpin@scout.wisc.edu>
2017-03-13 10:27:08 -0500

    ESLint fixes

--- pubdlcnt.js
+++ pubdlcnt.js
@@ -7,8 +7,9 @@
  * The administration JavaScript behavior.
  */
 (function ($) {
+  'use strict';
   Drupal.behaviors.pubdlcnt = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       var active = $('input[name="pubdlcnt_save_history"]:checked').attr('value');
       if (active) {
         $('#pubdlcnt-wrapper-period').show();
@@ -17,7 +18,7 @@
         $('#pubdlcnt-wrapper-period').hide();
       }
       // Switch the active provider with user input.
-      $('input[name="pubdlcnt_save_history"]').click(function() {
+      $('input[name="pubdlcnt_save_history"]').click(function () {
         var active = $('input[name="pubdlcnt_save_history"]:checked').attr('value');
         if (active) {
           $('#pubdlcnt-wrapper-period').show();
